fc25b39e388bb98dbab76b91571eabf0b6938c4a
Vinicius Nogueira <viniciushals@gmail.com>
2024-01-23 22:45:41 -0300

    novas alterações gerais

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,8 +1,103 @@
 // CSS
+import { useAuthentication } from "../../hooks/useAuthentication";
 import styles from "./Register.module.css";
 
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const [displayname, setDisplayname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      displayname,
+      email,
+      password,
+    };
+
+    if (password !== ConfirmPassword) {
+      setError("As senhas precisam ser iguais.");
+      return;
+    }
+
+    const res = await createUser(user);
+
+    console.log(user);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  return (
+    <div className={styles.register}>
+      <h1 className={styles.title}>Cadastre-se para postar</h1>
+      <p className={styles.TextInicial}>
+        Crie seu usuário abaixo e compartilhe suas histórias
+      </p>
+
+      <form onSubmit={handleSubmit} className={styles.FormRegister}>
+        <label className={styles.FormRegister__label}>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayname"
+            required
+            placeholder="Nome do usuário"
+            value={displayname}
+            onChange={(e) => setDisplayname(e.target.value)}
+          />
+        </label>
+        <label className={styles.FormRegister__label}>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className={styles.FormRegister__label}>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label className={styles.FormRegister__label}>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="ConfirmPassword"
+            required
+            placeholder="Confirme a sua senha"
+            value={ConfirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button disabled className="btn">Aguarde...</button>}
+      </form>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
 };
 
 export default Register;
