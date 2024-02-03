dab13504a7ad118887686b45c4ebeee102156863
Vinicius Nogueira <viniciushals@gmail.com>
2024-02-03 17:13:44 -0300

    novas alterações gerais

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -13,5 +13,48 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
-  
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) {
+        return;
+      }
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q;
+
+        q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+        await onSnapshot(q, (querySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      }
+
+      setLoading(false);
+    }
+
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  console.log(documents);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
 };
